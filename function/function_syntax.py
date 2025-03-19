ceabcf9bb827fea87194dcf927bd4ef04cc2c30c
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-03-19 07:12:49 +0530

    function

---
+++ function/function_syntax.py
@@ -0,0 +1,11 @@
+# Problem: Write a function to calculate and return the square of a number.
+
+n = int(input("Enter a number: "))
+
+def square (number):
+    return number ** 2
+
+result = square(n)
+print(result)
+
+
