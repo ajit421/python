40955ef5fb92cd06aed27e7fd8914ff64709b0dd
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-03-25 06:53:25 +0530

    exceptions

---
+++ error_handling/file.py
@@ -0,0 +1,9 @@
+# file = open('youtube.txt','w')
+# try:
+#     file.write('manage youtube video')
+# finally:
+#     file.close()
+
+
+with open('youtube.txt','w') as file:
+    file.write('Manage youtube video')
