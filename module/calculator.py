09f55489c5115a05dbbb259c0376348ae4cb434c
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-06-05 20:19:56 +0530

    module

---
+++ module/calculator.py
@@ -0,0 +1,41 @@
+# for square
+def square(num):
+    return num * num
+
+# for cube
+def cube(num):
+    return num**3
+
+# for factorial
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num-1)
+
+# for divide
+def divide(num1, num2):
+    return num1 / num2
+
+# for addition
+def add(num1, num2):
+    return num1 + num2
+
+# for substraction
+def sub(num1, num2):
+    return num1 - num2
+
+# for multiplication
+def multiplication(num1, num2):
+    return num1 * num2
+
+# for divide
+def divide(num1, num2):
+    return num1/num2
+
+
+    
+
+
+
+
