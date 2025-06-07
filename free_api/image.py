182831d7d6a5b12a352cc529ab185588309b5fc4
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-06-07 20:59:30 +0530

    api

--- free_api/image.py
+++ free_api/image.py
@@ -1,9 +1,12 @@
+# If you see "ModuleNotFoundError: No module named 'PIL'", install Pillow with:
+# pip install Pillow
+
 import requests
 from PIL import Image
 from io import BytesIO
 
 # Image URL
-url = "https://randomuser.me/api/portraits/women/59.jpg"
+url = "https://randomuser.me/api/portraits/women/65.jpg"
 
 # Fetch the image
 response = requests.get(url)
