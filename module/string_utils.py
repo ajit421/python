933bcc7b9fd89bff7e320131c581edb15aa773b9
Ajit kumar <110296927+ajit421@users.noreply.github.com>
2025-06-08 14:31:14 +0530

    string
    future plan

---
+++ module/string_utils.py
@@ -0,0 +1,19 @@
+# reversal of a string 
+def rev(string):
+    return string[::-1]
+
+
+# palidrome check
+def palidrome(string):
+    pal = string[::-1]
+    return pal == string
+
+
+# concatenation of two strinngs
+def concatenation(string1, string2):
+    return string1 + string2
+
+# string length
+def length(string):
+    return len(string)
+
